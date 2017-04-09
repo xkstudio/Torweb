a9ab7965b925c0ed3997c5eed7924cf1d088c231
luxiaok <me@luxiaok.com>
2017-04-09 17:03:05 +0800

    Improve Log

--- app/Torweb.py
+++ app/Torweb.py
@@ -10,7 +10,6 @@ import tornado.netutil
 import tornado.process
 import platform
 import tornado.options
-from Log import Log
 from db import DB
 from db import Redis
 from handler.page import Page404Handler
@@ -24,7 +23,7 @@ class App(tornado.web.Application):
         settings['default_handler_class'] = Page404Handler # 404
         tornado.web.Application.__init__(self, handlers, **settings)
         #后台日志高亮输出
-        tornado.options.parse_command_line()
+        #tornado.options.parse_command_line()
         #每10秒执行一次
         #tornado.ioloop.PeriodicCallback(self.test, 1 * 10 * 1000).start()
         self.__version__ = conf['version']
@@ -43,8 +42,6 @@ class Torweb():
 
     def __init__(self,processes=4):
         self.__version__ = '0.0.1'
-        _log = Log()
-        self.log = _log.info
         self.host = config['host']
         self.port = config['port']
         self.urls = route
@@ -58,8 +55,9 @@ class Torweb():
 
     #多线程模式
     def run(self):
-        self.log('Torweb %s' % self.__version__) # 启动时打印版本号
-        self.log('Listen Port: %s' % self.port)
+        tornado.options.parse_command_line()
+        #self.log('Torweb %s' % self.__version__) # 启动时打印版本号
+        #self.log('Listen Port: %s' % self.port)
         http_sockets = tornado.netutil.bind_sockets(self.port, self.host)
         tornado.process.fork_processes(num_processes=self.processes)
         http_server = tornado.httpserver.HTTPServer(request_callback=App(self.urls,self.settings,self.config), xheaders=True)
