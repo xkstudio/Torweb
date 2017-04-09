eb37fb86c446ffcb3acd4bb1732db987cdc054e6
luxiaok <me@luxiaok.com>
2017-04-09 12:06:27 +0800

    Add IndexHandler

---
+++ handler/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# Powered By KK Studio
+# Index Page
+
+from BaseHandler import BaseHandler
+from tornado.web import authenticated as Auth
+
+class IndexHandler(BaseHandler):
+
+    @Auth
+    def get(self):
+        self.render('index/index.html')
