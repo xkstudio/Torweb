7aacd74c66d68cc3d42d1f5fb285735423c46d62
luxiaok <me@luxiaok.com>
2017-04-09 17:08:56 +0800

    Improve Log Function

--- app/Torweb.py
+++ app/Torweb.py
@@ -10,6 +10,7 @@ import tornado.netutil
 import tornado.process
 import platform
 import tornado.options
+from tornado.log import gen_log
 from db import DB
 from db import Redis
 from handler.page import Page404Handler
@@ -19,14 +20,13 @@ from handler import route
 
 class App(tornado.web.Application):
 
-    def __init__(self,handlers,settings,conf):
-        settings['default_handler_class'] = Page404Handler # 404
+    def __init__(self,handlers,settings,conf,log):
+        self.__version__ = conf['version']
+        self.log = log
+        settings['default_handler_class'] = Page404Handler  # 404
         tornado.web.Application.__init__(self, handlers, **settings)
-        #后台日志高亮输出
-        #tornado.options.parse_command_line()
         #每10秒执行一次
         #tornado.ioloop.PeriodicCallback(self.test, 1 * 10 * 1000).start()
-        self.__version__ = conf['version']
         #封装数据库
         _db = conf['db']
         self.db = DB(_db['host'],_db['port'],_db['user'],_db['pass'],_db['db'],_db['charset'])
@@ -48,6 +48,7 @@ class Torweb():
         self.settings = config['app_settings']
         self.config = config
         self.config['version'] = self.__version__
+        self.log = gen_log
         if platform.system() == "Linux":  #根据操作系统类型来确定是否启用多线程
             self.processes = processes # 当processes>1时，PeriodicCallback定时任务会响相应的执行多次
         else:
@@ -56,10 +57,10 @@ class Torweb():
     #多线程模式
     def run(self):
         tornado.options.parse_command_line()
-        #self.log('Torweb %s' % self.__version__) # 启动时打印版本号
-        #self.log('Listen Port: %s' % self.port)
+        self.log.info('Torweb %s' % self.__version__)  # 启动时打印版本号
+        self.log.info('Listen Port: %s' % self.port)
         http_sockets = tornado.netutil.bind_sockets(self.port, self.host)
         tornado.process.fork_processes(num_processes=self.processes)
-        http_server = tornado.httpserver.HTTPServer(request_callback=App(self.urls,self.settings,self.config), xheaders=True)
+        http_server = tornado.httpserver.HTTPServer(request_callback=App(self.urls,self.settings,self.config,self.log), xheaders=True)
         http_server.add_sockets(http_sockets)
         tornado.ioloop.IOLoop.instance().start()
