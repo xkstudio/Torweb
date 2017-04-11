2b6f3cb96fe451f159d691e4af147f6dd4f2923d
luxiaok <me@luxiaok.com>
2017-04-12 00:21:55 +0800

    Add Function _on_finish

--- handler/BaseHandler.py
+++ handler/BaseHandler.py
@@ -23,11 +23,17 @@ class BaseHandler(tornado.web.RequestHandler):
         # Current Route
         self.url = self.get_current_route()
 
-    # 重载on_finish
-    def on_finish(self):
+    # 后面的方法如果重写on_finish方法，需要调用_on_finish
+    def _on_finish(self):
+        # 更新Session
+        if self.session:
+            self.save_session()  # 更新Session到Redis
         # 请求逻辑处理结束时关闭数据库连接，如果不关闭可能会造成MySQL Server has gone away 2006错误
         #self.db.close()
-        pass
+
+    # 重载on_finish
+    def on_finish(self):
+        self._on_finish()
 
     # 重载write_error方法
     def write_error(self, status_code, **kwargs):
