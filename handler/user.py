3df326df29e1f2751d38fe2fc8621ac3315155de
luxiaok <me@luxiaok.com>
2017-04-15 16:35:42 +0800

    Add create_session method

--- handler/user.py
+++ handler/user.py
@@ -9,6 +9,20 @@ class LoginHandler(BaseHandler):
     def get(self):
         self.render('user/login.html')
 
+    def post(self):
+        pass
+
+    def create_session(self,data,remember,cookie_name):
+        sid = self.session.gen_session_id()
+        self.session.data = data
+        self.session.isGuest = False
+        #self.session.save() # Why don't save? See self._on_finish !!
+        if remember == "yes":
+            expires_days = 15  # Remember Session 15 days
+        else:
+            expires_days = None
+        self.set_secure_cookie(cookie_name, sid, expires_days)
+
 class RegisterHandler(BaseHandler):
 
     def get(self):
